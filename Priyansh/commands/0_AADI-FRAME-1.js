1fb892de506b478181e8c4a61c67e14ad1e649b2
MD SIRAJ <siiraaj16@gmail.com>
2025-07-14 00:00:59 +0300

    Update 0_AADI-FRAME-1.js

--- Priyansh/commands/0_AADI-FRAME-1.js
+++ Priyansh/commands/0_AADI-FRAME-1.js
@@ -3,7 +3,7 @@ module.exports.config = {
     version: "7.3.1",
     hasPermssion: 0,
     credits: "AADI BABU", 
-    description: "THIS BOT WAS MADE BY MR AADI BABU",
+    description: "THIS BOT WAS MADE BY MR SAMEER KHAN",
     commandCategory: "PROFILE DP FRAME",
     usages: "PREFIX MENTIONS",
     cooldowns: 5,
