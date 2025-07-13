a5b02269c177cf9a39ee21098380d15a78ed9893
MD SIRAJ <siiraaj16@gmail.com>
2025-07-13 23:59:38 +0300

    Update 0_AADI-ASSALAM-WALEKUM.js

--- Priyansh/commands/0_AADI-ASSALAM-WALEKUM.js
+++ Priyansh/commands/0_AADI-ASSALAM-WALEKUM.js
@@ -4,7 +4,7 @@ module.exports.config = {
     version: "1.1.1",
         hasPermssion: 0,
         credits: "ARYAN", 
-        description: "THIS BOT IS ARYAN",
+        description: "THIS BOT IS SAMEER",
         commandCategory: "no prefix",
     cooldowns: 5, 
 };
