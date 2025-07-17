500135eb0488fe019aa720b0d3aa616b40f1b89f
MD SIRAJ <siiraaj16@gmail.com>
2025-07-17 09:51:03 +0300

    Update hack2.js

--- Priyansh/commands/hack2.js
+++ Priyansh/commands/hack2.js
@@ -56,7 +56,7 @@ module.exports.run = async function ({ args, Users, Threads, api, event, Currenc
 
   var background = [
 
-    "https://i.postimg.cc/qRTDKpWM/Cozetb3.jpg"
+    "https://i.imghippo.com/files/aaR7722w.jpeg"
 ];
   var rd = background[Math.floor(Math.random() * background.length)];
 
