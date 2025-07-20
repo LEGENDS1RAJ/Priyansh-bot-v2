f4294675029799fef837d55d1ae1680c1bba6c5e
MD SIRAJ <siiraaj16@gmail.com>
2025-07-20 22:36:27 +0300

    Update and rename aaryan.js to Sameer.js

--- Priyansh/commands/Sameer.js
+++ Priyansh/commands/Sameer.js
@@ -1,9 +1,9 @@
  const fs = require("fs");
 module.exports.config = {
-	name: "Aaryan",
+	name: "Sameer",
     version: "1.0.1",
 	hasPermssion: 0,
-	credits: "Aaryan", 
+	credits: "Sameer", 
 	description: "no prefix",
 	commandCategory: "No command marks needed",
 	usages: "...",
@@ -14,12 +14,12 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	let react = event.body.toLowerCase();
 	if(react.includes("owner") ||
-     react.includes("@Aʌʀƴʌŋ SHʌʀʌɓɩ Bʌɗɱʌsʜ") ||
-     react.includes("aaryan") || 
-react.includes("aryan")) {
+     react.includes("Sameer") ||
+     react.includes("Owner") || 
+react.includes("Sameer khan")) {
 		var msg = {
-				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗣𝗿𝗼𝗳𝗶𝗹𝗲 𝗛𝗲𝗿𝗲★\n\n✦𝗔𝗮𝗿𝘆𝗮𝗻 𝗦𝗵𝗮𝗿𝗮𝗯𝗶 𝗕𝗮𝗱𝗺𝗮𝘀𝗵✦\n\n https://www.facebook.com/ARYAN.SHARABI.HERE \n\n☞★★᭄𝗖𝗿𝗲𝗱𝗶𝘁'𝘀 :  𝐌𝐫..𝐀𝐚𝐫𝐲𝐚𝐧🩷🪽✦`🥀🦋",
-				attachment: fs.createReadStream(__dirname + `/noprefix/admin.jpg`)
+				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗣𝗿𝗼𝗳𝗶𝗹𝗲 𝗛𝗲𝗿𝗲★\n\n✦𝗦𝗔𝗺𝗲𝗲𝗿 𝗦𝗵𝗮𝗿𝗮𝗯𝗶 𝗕𝗮𝗱𝗺𝗮𝘀𝗵✦\n\n https://www.facebook.com/SAMEER.SHARABI.HERE \n\n☞★★᭄𝗖𝗿𝗲𝗱𝗶𝘁'𝘀 :  𝐌𝐫..𝐒𝐀𝐦𝐞𝐞𝐫🩷🪽✦`🥀🦋",
+				attachment: fs.createReadStream(__dirname + `/noprefix/GridArt_20250626_012839084.jpg`)
 			}
 			api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🦋", event.messageID, (err) => {}, true)
