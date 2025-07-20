9343fee0ab0e2e1368095b65a5813c50aa95bf0b
MD SIRAJ <siiraaj16@gmail.com>
2025-07-20 22:31:00 +0300

    Update and rename Fram.js to Fram6.js

--- Priyansh/commands/Fram6.js
+++ Priyansh/commands/Fram6.js
@@ -22,7 +22,7 @@ module.exports.onLoad = async() => {
     const dirMaterial = __dirname + `/cache/canvas/`;
     const path = resolve(__dirname, 'cache/canvas', 'fram.jpg');
     if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
-    if (!existsSync(path)) await downloadFile("https://i.postimg.cc/cCGnskcX/images-1-2.jpg", path);
+    if (!existsSync(path)) await downloadFile("https://i.imghippo.com/files/lUvz4415ouE.jpg", path);
 }
 
 async function makeImage({ one, two }) {
