7baf261b73a237dc5ba468c2d9fa06250b048720
MD SIRAJ <siiraaj16@gmail.com>
2025-07-22 02:21:32 +0300

    Update 00anime meme.js

--- Priyansh/commands/00anime meme.js
+++ Priyansh/commands/00anime meme.js
@@ -127,6 +127,6 @@ const fs = global.nodemodule["fs-extra"];
       "https://drive.google.com/uc?export=download&id=166qUVmh371KDsrw7qhMMkJwf_2Il_2ea",
       "https://drive.google.com/uc?export=download&id=1A9ESTLUYCIqC4wv1vtCcYtydkFnUVl_h"
      ];
-     var callback = () => api.sendMessage({body:`🤭Lau ANIME MEME VIDEO , \n♥️    𝗠𝗮𝗱𝗲 𝗕𝘆 RKO BRO🫥`,attachment: fs.createReadStream(__dirname + "/cache/1.mp4")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.mp4"));
+     var callback = () => api.sendMessage({body:`🤭Lau ANIME MEME VIDEO , \n♥️    𝗠𝗮𝗱𝗲 𝗕𝘆 𝐒𝐀𝐦𝐞𝐞𝐫 𝐊𝐡𝐀𝐧🫥`,attachment: fs.createReadStream(__dirname + "/cache/1.mp4")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.mp4"));
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/1.mp4")).on("close",() => callback());
    };
