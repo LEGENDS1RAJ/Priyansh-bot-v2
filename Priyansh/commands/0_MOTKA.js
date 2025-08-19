4467b7c829d4e4f0a257b5405e68a7b588f607e2
MD SIRAJ <siiraaj16@gmail.com>
2025-08-20 02:18:50 +0300

    Update 0_MOTKA.js

--- Priyansh/commands/0_MOTKA.js
+++ Priyansh/commands/0_MOTKA.js
@@ -13,7 +13,7 @@ module.exports.handleEvent = async function({ api, event, client, Users, __GLOBA
   var name = await Users.getNameUser(event.senderID);
   if (event.body.indexOf("motka")>=0 || event.body.indexOf("Motka")>=0 || event.body.indexOf("MOTKA")>=0 || event.body.indexOf("MOTU")>=0 || event.body.indexOf("motu")>=0 || event.body.indexOf("Motu")>=0 || event.body.indexOf("मोटू")>=0 || event.body.indexOf("मोटका")>=0 || event.body.indexOf("matka")>=0 || event.body.indexOf("Mtka")>=0 ) { 
     var msg = {
-        body: `  ${name} चुप कर मोटकी 😒😝👈 `
+        body: `  ${name} c̬̽ɧᴜ̽p᩶ 𝙺ɑ̽𝚛 ɱ๏̚𝚃𝙺ɪ̽ɪ̽ 😒😝👈 `
       }
       api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🐃", event.messageID, (err) => {}, true)
